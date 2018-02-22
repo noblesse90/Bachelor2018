d42c1de1121df47c0d4a374c82c1312ed098b329
noblesse90 <36155020+noblesse90@users.noreply.github.com>
2018-02-22 11:10:48 +0100

    Immortal Guardians V1

---
+++ Immortal Guardians V1/Immortal Guardians V1/Assets/Script/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public int speed = 3;
+
+    public Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        var x = Input.GetAxis("Horizontal") * speed;
+        var y = Input.GetAxis("Vertical") * speed;
+
+        Vector3 movement = new Vector3(x, y, 0);
+
+        
+        if (rb.velocity.magnitude > speed)
+        {
+            rb.velocity = rb.velocity.normalized * speed;
+        }
+        else
+        {
+            rb.velocity = movement;
+        }
+
+
+
+    }
+}
